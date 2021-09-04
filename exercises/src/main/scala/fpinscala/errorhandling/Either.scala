26c291354d1d6d48987028fe35c8229c4dff5677
iltonmi <linweili666@163.com>
2021-09-04 17:08:36 +0800

    corrected answers of ch4 Either

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -6,44 +6,47 @@ import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std li
 sealed trait Either[+E,+A] {
  def map[B](f: A => B): Either[E, B] = this match {
    case Right(a) => Right(f(a))
+   case Left(e) => Left(e)
  }
 
  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
    case Right(a) => f(a)
+   case Left(e) => Left(e)
  }
 
  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
    case Left(_) => b
-   case Right(_) => this
+   case Right(a) => Right(a)
  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
    this flatMap (aa => b flatMap (bb => Right(f(aa,bb))))
- }
+
+  def map2UisingForComprehension[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {a1 <- this; b1 <- b} yield { f(a1, b1) }
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
-    Right((es map f).foldLeft(List())((t,x) => {
-      x match {
-        case Left(e) => return Left(e)
-        case Right(a) => t.appended(a)
-      }
-    }))
+  // still 2 ways like in Opion: foldRight or recursion
+
+  // tips:
+  // use map2 to merge 2 Eithers of different types...
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E, List[B]]](Right(Nil))((a,b) => f(a).map2(b)(_::_))
+
+  // recursion
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match {
+    case Nil => Right(Nil)
+    case h::t => f(h).map2(traverse(t)(f))(_::_)
   }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = {
-    Right(
-      es.foldLeft(List())((t,x) => {
-        x match {
-          case Left(e) => return Left(e)
-          case Right(a) => t.appended(a)
-        }
-      })
-    )
-  }
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    es.foldRight[Either[E,List[A]]](Right(Nil))((a,b) => a.map2(b)(_::_))
+
+  def sequence_1[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(x => x)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
