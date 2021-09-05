ab9cc688f9569df7edac90f3c2c74cacd496688d
iltonmi <linweili666@163.com>
2021-09-05 16:22:32 +0800

    corrected answers for ch5 Stream

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -22,32 +22,24 @@ trait Stream[+A] {
   def toList() : List[A] =
     foldRight(List[A]())(_::_)
 
-  def take(n: Int): Stream[A] = {
-    def go(n : Int, s: Stream[A]) : Stream[A] = s match {
-      case Cons(h,t) if n > 0 => Cons(h, () => go(n-1,t()))
-      case _ => empty
-    }
-    go(n,this)
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h,t) if n > 0 => cons(h(), t().take(n-1))
+    case _ => empty
   }
 
-  def drop(n: Int): Stream[A] = {
-    def go(n: Int, s: Stream[A]) : Stream[A] = s match {
-      case s@Cons(_,t) => if(n > 0) go(n-1,t()) else s
-      case _ => empty
-    }
-    go(n, this)
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_,t) if(n > 0) => t().drop(n-1)
+    case _ => this
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = {
-    def go(s: Stream[A]) : Stream[A] = s match {
-      case Cons(h,t) if p(h()) => Cons(h, () => go(t()))
-      case _ => empty
-    }
-    go(this)
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
   }
 
+  // remember right parameter of foldRight is the folded value
   def takeWhile1(p: A => Boolean) : Stream[A] =
-    foldRight(empty[A])((a,b) => if(p(a)) Cons(() => a, () => b.takeWhile1(p)) else empty)
+    foldRight(empty[A])((a,b) => if(p(a)) cons(a, b) else empty)
 
   def forAll(p: A => Boolean): Boolean =
     foldRight(true)((a,b) => p(a) && b)
@@ -60,26 +52,31 @@ trait Stream[+A] {
   def map[B](f: A => B) : Stream[B] =
     foldRight(empty[B])((a,b) => cons(f(a), b))
 
+  // remember right parameter of foldRight is folded value
   def filter(p: A => Boolean) : Stream[A] =
-    foldRight(empty[A])((a,b) => if(p(a)) cons(a, b.filter(p)) else b.filter(p))
+    foldRight(empty[A])((a,b) => if(p(a)) cons(a, b) else b)
 
+  // use appended value as default value directly
+  // unnecessary to check default value and replace it with s at the right end
   def append[B>:A](s: Stream[B]) : Stream[B] =
-    foldRight(empty[B])((a,b) => if(b == empty) cons(a, s) else cons(a, b))
+    foldRight(s)((a,b) => cons(a, b))
 
+  // use append to concat the single value stream and tail stream
+  // rather than use head option to get the single value and
+  // use the smart constructor to assemble single value and tail stream
   def flapMap[B](f: A => Stream[B]) : Stream[B] =
-    foldRight(empty[B])((a,b) => cons(f(a).headOption.get, b))
+    foldRight(empty[B])((a,b) => f(a) append b)
 
   def map_1[B](f: A => B): Stream[B] = unfold(this) {
     case Cons(h, t) => Some((f(h()), t()))
-    case Empty => None
+    case _ => None
   }
 
-  def take_1(n: Int): Stream[A] = unfold((this, n))(z => {
-    z._1 match {
-      case Cons(h,t) if z._2 > 0 => Some((h(), (t(), z._2-1)))
-      case _ => None
-    }
-  })
+  // use tuple directly for pattern matching
+  def take_1(n: Int): Stream[A] = unfold((this, n)) {
+    case (Cons(h,t), n) if n > 0 => Some((h(), (t(), n-1)))
+    case _ => None
+  }
 
   def takeWhile_1(p: A => Boolean): Stream[A] = unfold(this) {
     case Cons(h, t) if p(h()) => Some(h(), t())
@@ -91,6 +88,11 @@ trait Stream[+A] {
     case _ => None
   }
 
+  // it seems that this is not a exercise in the book
+  // special case of `zipWith`
+  def zip[B](s2: Stream[B]): Stream[(A,B)] =
+    zipWith(s2)((_,_))
+
   def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = unfold((this, s2)) {
     case (Cons(h1,t1), Cons(h2,t2)) => Some(((Some(h1()),Some(h2())), (t1(),t2())))
     case (Cons(h1,t1), Empty) => Some(( (Some(h1()), None), (t1(), empty) ))
@@ -98,6 +100,23 @@ trait Stream[+A] {
     case _ => None
   }
 
+  // official answer:
+  // abstract one more layer deeper to zip 2 option2 in zipWithAll
+  // but zipWithAll is not a exercise anyway
+  def zipAll_1[B](s2: Stream[B]): Stream[(Option[A],Option[B])] =
+    zipWithAll(s2)((_,_))
+
+  // zip option tuple2
+  def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+    Stream.unfold((this, s2)) {
+      case (Empty, Empty) => None
+      case (Cons(h, t), Empty) => Some(f(Some(h()), Option.empty[B]) -> (t(), empty[B]))
+      case (Empty, Cons(h, t)) => Some(f(Option.empty[A], Some(h())) -> (empty[A] -> t()))
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
+    }
+
+  // my answers:
+  // use shortcut of foldRight directly
   def startsWith[B](s: Stream[B]): Boolean =
     zipAll(s).foldRight(true)( (o,v) => o match {
       case (Some(a), Some(b)) => if(a == b) v else false
@@ -105,11 +124,23 @@ trait Stream[+A] {
       case (None, _) => false
     } )
 
+  // offical answers:
+  // reuse takeWhile to cut the stream to the length of prefix first
+  // then match each value from left in turns
+  def startsWith_1[B](s: Stream[B]): Boolean =
+    zipAll(s).takeWhile(!_._2.isEmpty) forAll (o => o._1 == o._2)
+
+  // My answers with a little difference with official answers
+  // In my consideration, on one hand, unfold will return at least emtpy stream
+  // on the other hand, a nonempty stream always contains empty stream at tail
+  // As conclusion, it is unnecessary to append empty stream after calling unfold.
   def tails: Stream[Stream[A]] = unfold(this) {
     case s@Cons(_, t) => Some((s, t()))
     case _ => None
   }
 
+  // my answer 1:
+  // recursively call scanRight without duplicate evaluation
   def scanRight[B](z: => B)(f: (A, => B) => B): Stream[B] = this match {
     case Empty => Stream(z)
     case Cons(h,t) => {
@@ -118,9 +149,29 @@ trait Stream[+A] {
     }
   }
 
+  def scanRight_o[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      // p0 is passed by-name and used in by-name args in f and cons. So use lazy val to ensure only one evaluation...
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
+
+  // fix my answer 2 using foldRight with lazy variable to cache
   def scanRight_1[B](z: => B)(f: (A, => B) => B): Stream[B] = {
-    foldRight(Stream(z))((a,s) => cons(f(a,s.headOption.get), s))
+    foldRight(Stream(z))((a,s) => {
+      lazy val s1 = s
+      cons(f(a,s1.headOption.get), s1)
+    })
   }
+
+  // by the way, one of biggest difference between the official and mine
+  // is that they didn't use head Option to extract the first value of tail stream
+  // but store extra first value of stream in the folded value as a tuple with the tail stream
+  // fine, official answer is more natural while my answer still need to indirectly use foldRight
+  // by using headOption to extract the head value
+  // both way is okay, the official one is more autonomy
+
 
   def hasSubsequence[A](s: Stream[A]): Boolean =
     tails exists (_ startsWith s)
@@ -150,7 +201,15 @@ object Stream {
     cons(a, constant(a))
 
   def constant_1[A](a: A): Stream[A] =
-    unfold(a)(s => Some(s,s))
+    unfold(a)(_ => Some(a,a))
+
+  // official answer:
+  // This is more efficient than `cons(a, constant(a))` since it's just
+  // one object referencing itself.
+  def constant_2[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = cons(a, tail)
+    tail
+  }
 
   def from(n: Int): Stream[Int] =
     cons(n, from(n+1))
@@ -164,8 +223,22 @@ object Stream {
     cons(0,cons(1, go(0,1)))
   }
 
+  // official way
+  def fibs_2() : Stream[Int] = {
+    def go(x: Int, y: Int) : Stream[Int] =
+      cons(x, go(y,x+y))
+    go(0,1)
+  }
+
   def fibs_1() : Stream[Int] =
     unfold((0,1))(s => Some(s._1, (s._2, s._1 + s._2)))
+
+  /*
+  Scala provides shorter syntax when the first action of a function literal is to match on an expression.
+  The function passed to `unfold` in `fibsViaUnfold` is equivalent to `p => p match { case (f0,f1) => ... }`, but we avoid having to choose a name for `p`, only to pattern match on it.
+  */
+  val fibsViaUnfold =
+    unfold((0,1)) { case (f0,f1) => Some((f0,(f1,f0+f1))) }
 
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
@@ -173,8 +246,23 @@ object Stream {
     case Some((x,y)) => cons(x, unfold(y)(f))
   }
 
+  /*
+  The below two implementations use `fold` and `map` functions in the Option class to implement unfold, thereby doing away with the need to manually pattern match as in the above solution.
+   */
+  // 2 unfold implementations from official
+  // the meaning is learn to use Option in scala library to
+  // fold value
+  def unfoldViaFold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z).fold(empty[A])((p: (A,S)) => cons(p._1,unfold(p._2)(f)))
+
+  def unfoldViaMap[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z).map((p: (A,S)) => cons(p._1,unfold(p._2)(f))).getOrElse(empty[A])
+
   def main(args: Array[String]): Unit = {
+    println(Stream(1,2,3).take(1).toList())
     println(Stream(1,2,3).take(2).toList())
+    println(Stream(1,2,3).take(3).toList())
+    println(Stream(1,2,3).take(5).toList())
     println(Stream(1,2,3).drop(2).toList())
     println(Stream(1,2,3).takeWhile_1(_%2 == 0).toList())
     println(Stream(1,2,3).takeWhile_1(_%2 == 1).toList())
@@ -190,9 +278,10 @@ object Stream {
     println(ones.forAll(_ != 1))
     println(fibs().take(10).toList())
     println(fibs_1().take(10).toList())
+    println(fibs_2().take(10).toList())
     println(from(11).take(10).toList())
     println(from_1(11).take(10).toList())
-    println(constant(11).take_1(10).toList())
+    println(constant_2(11).take_1(10).toList())
     println(constant_1(11).take_1(10).toList())
     println(ones.map_1(_ => 2).take_1(10).toList())
 
@@ -210,7 +299,19 @@ object Stream {
     println(Empty.hasSubsequence(Empty))
     println(Empty.hasSubsequence(ones.take(1)))
 
-    println(Stream(1,2,3).scanRight(0)(_ + _).toList)
-    println(Stream(1,2,3).scanRight_1(0)(_ + _).toList)
+    println(Stream(1,2,3).scanRight(0)((a,b) => {
+      println(a + ":" + b)
+      a + b
+    }).toList)
+    println(Stream(1,2,3).scanRight_1(0)((a,b) => {
+      println(a + ":" + b)
+      a + b
+    }).toList)
+    println(Stream(1,2,3).scanRight_o(0)((a,b) => {
+      println(a + ":" + b)
+      a + b
+    }).toList)
+
+    println((Stream(),Stream()))
   }
 }
